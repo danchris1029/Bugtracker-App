d4c4799f70875eaf5b3c70610163eadc9e933e26
unknown <cyanhawkk@github.com>
2021-08-02 01:20:38 -0500

    update

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ router.post('/save', (req, res) =>{
 
 app.use(express.static(path.join(__dirname, 'client')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    console.log(path.join(__dirname, '/client/build/index.html'));
+});
 
 router.get("/", (req, res) => {
     Issues.find({})
